142de8e8304062554698f1f25cfddec0ff054ac0
Blak3Nick <blaken2021@gmail.com>
2018-10-21 09:27:07 -0400

    Added new methods

---
+++ src/Squat.java
@@ -0,0 +1,12 @@
+import java.util.Date;
+
+public class Squat {
+    private double oneRepMax;
+    private String variations[] = new String[20];
+    Date lastSquatSession;
+
+    public void addLastSquatSession(Date date, String variation) {
+
+    }
+
+}
